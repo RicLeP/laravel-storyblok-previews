856823593637d9c9d76ab2053ea31fb2ff61b003
Richard Le Poidevin <ric@sirric.co.uk>
2024-12-04 12:48:56 +0000

    Refactoring and new features
    Refactored the code
    Added the ability to set screen size per component and set the default
    Single components can now be captured by passing their name to the command
    Component list can now just be names and it will automatically search for CSS classes that match

--- config/storyblok-previews.php
+++ config/storyblok-previews.php
@@ -2,23 +2,31 @@
 
 return [
 
-	/*
-	|--------------------------------------------------------------------------
-	| Components
-	|--------------------------------------------------------------------------
-	|
-	| Array of components to generate screenshots for. The key should match the
-	| component name in Storyblok. Each component should have a selector to
-	| target the component in the HTML. Optionally, you can specify a filename
-	| for the screenshot, a URL to navigate to and a delay in milliseconds to
-	| wait before taking the screenshot.
-	|
-	| If you leave the filename empty, the component name will be used.
-	| If you don't specify a URL it will try and find a published page in
-	| Storyblok that contains the component.
-	|
-	| Example:
-	    [
+    /*
+    |--------------------------------------------------------------------------
+    | Components
+    |--------------------------------------------------------------------------
+    |
+    | Array of components to generate screenshots for. If you only pass a value
+    | without any settings it will try and find a component on your website
+    | with a CSS class matching the component name.
+    |
+    | For more control use an array with the component name as the key and
+    | pass your settings as detailed below.
+    | delay: optional, time in milliseconds to wait before taking the screenshot
+    | filename: optional, the filename for the screenshot
+    | selector: required, the CSS selector to target the component in the HTML
+    | url: optional, the URL to navigate to before taking the screenshot
+    | window_size: optional, the window size to use when taking the screenshot
+    |
+    | If you leave the filename empty, the component name will be used.
+    | If you don't specify a URL it will try and find a page that uses the
+    | component.
+    | If you don't specify a window size it will use the default window size.
+    |
+    | Example:
+        [
+            'name',
             'hero' => [
                 'selector' => '.hero',
             ],
@@ -27,41 +35,66 @@ return [
                 'filename' => 'grid.jpg',
                 'selector' => ':has(> .grid),
                 'url' => '/about',
+                'window_size' => [500, 200],
+
             ],
         ];
-	|
-	*/
-	'components' => [],
+    |
+    */
+    'components' => [
+    ],
 
-	/*
-	|--------------------------------------------------------------------------
-	| Base URL
-	|--------------------------------------------------------------------------
-	|
-	| The base URL for your application. This is used to generate the preview
-	| URLs, i.e https://uandus.co.uk/
-	|
-	*/
-	'base_url' => '',
+    /*
+    |--------------------------------------------------------------------------
+    | Allow only published stories
+    |--------------------------------------------------------------------------
+    |
+    | If set to true, only published stories will be used to generate the
+    | screenshots. If set to false it will use every story, including drafts.
+    |
+    */
+    'only_published' => false,
 
-	/*
-	|--------------------------------------------------------------------------
-	| Screenshot Storage Path
-	|--------------------------------------------------------------------------
-	|
-	| The storage path for temporarily storing the screenshots.
-	|
-	*/
-	'storage_path' => '/storyblok/previews/',
-	/*
-	|--------------------------------------------------------------------------
-	| Screenshot Prefix
-	|--------------------------------------------------------------------------
-	|
-	| Prefix for the screenshot files.
-	|
-	*/
-	'prefix' => 'preview-',
+    /*
+    |--------------------------------------------------------------------------
+    | Window Size (width, height)
+    |--------------------------------------------------------------------------
+    |
+    | The window size to use when taking the screenshots, it can be overridden
+    | by specifying a size in the component configuration.
+    |
+    */
+    'default_window_size' => [1400, 1200],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base URL
+    |--------------------------------------------------------------------------
+    |
+    | The base URL for your application. This is used to generate the preview
+    | URLs, i.e https://uandus.co.uk/
+    |
+    */
+    'base_url' => 'https://beautiful-tundra-y9wkj8wgzl.ploi.dev/',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Screenshot Storage Path
+    |--------------------------------------------------------------------------
+    |
+    | The storage path for temporarily storing the screenshots.
+    |
+    */
+    'storage_path' => '/storyblok/previews/',
+    /*
+    |--------------------------------------------------------------------------
+    | Screenshot Prefix
+    |--------------------------------------------------------------------------
+    |
+    | Prefix for the screenshot files.
+    |
+    */
+    'prefix' => 'preview-',
 
     /*
     |--------------------------------------------------------------------------
--- src/Console/CreateBlockPreviewsCommand.php
+++ src/Console/CreateBlockPreviewsCommand.php
@@ -13,161 +13,91 @@ use Wnx\SidecarBrowsershot\BrowsershotLambda;
 
 class CreateBlockPreviewsCommand extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'ls:make-previews';
+    protected $signature = 'ls:make-previews {componentName?}';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Creates previews for components by capturing screenshots of the components on the live site.';
 
+    protected ManagementClient $client;
 
-    /**
-     * Execute the console command.
-     */
+    protected array $toCapture;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->client = new ManagementClient(config('storyblok-cli.oauth_token'));
+        $this->toCapture = config('storyblok-previews.components');
+    }
+
     public function handle()
     {
-        $client = new ManagementClient(config('storyblok-cli.oauth_token'));
-
-        // TODO - set view port size? per component?
-        $toCapture = config('storyblok-previews.components');
-        $spaceId = config('storyblok-cli.space_id');
-        $baseUrl = config('storyblok-previews.base_url');
-        $screenshotPath = config('storyblok-previews.storage_path');
-        $screenshotPrefix = config('storyblok-previews.prefix');
-        $screenshotAssetFolder = config('storyblok-previews.folder');
-
         $capturedComponents = [];
         $uncapturedComponents = [];
 
-        Storage::makeDirectory($screenshotPath);
+        Storage::makeDirectory(config('storyblok-previews.storage_path'));
 
-        $componentsData = Components::make()
-            ->all()
-            ->getComponents();
+        $assetFolder = $this->getOrCreateAssetFolder();
 
-        $assetFolders = AssetFolders::make()
-            ->all()
-            ->getAssetFolders();
-        $assetFolder = $assetFolders->where('name', $screenshotAssetFolder)->first();
+        $componentName = $this->argument('componentName');
+
+        // loop through all the components and update any that are just string values with the default selector
+        foreach ($this->toCapture as $componentKey => $captureSettings) {
+            if (is_string($captureSettings)) {
+                unset($this->toCapture[$componentKey]);
+
+                $this->toCapture[$captureSettings] = ['selector' => '.' . $captureSettings];
+
+                ksort($this->toCapture);
+            }
+        }
+
+        if ($componentName) {
+            $this->processSingleComponent($componentName, $assetFolder, $capturedComponents, $uncapturedComponents);
+        } else {
+            $this->processAllComponents($assetFolder, $capturedComponents, $uncapturedComponents);
+        }
+
+        $this->displayResults($capturedComponents, $uncapturedComponents);
+    }
+
+    protected function getOrCreateAssetFolder()
+    {
+        $assetFolders = AssetFolders::make()->all()->getAssetFolders();
+        $assetFolder = $assetFolders->where('name', config('storyblok-previews.folder'))->first();
 
         if (!$assetFolder) {
             $assetFolder = AssetFolders::make()->create([
                 'asset_folder' => [
-                    'name' => $screenshotAssetFolder,
+                    'name' => config('storyblok-previews.folder'),
                     'parent_id' => null,
                 ],
             ])->getAssetFolder()->toArray();
         }
 
-        foreach ($toCapture as $componentName => $captureSettings) {
-            $component = $componentsData->where('name', $componentName)->first();
+        return $assetFolder;
+    }
 
-            if ($component) {
-                if (array_key_exists('url', $captureSettings)) {
-                    $componentPageUrl = $baseUrl . $captureSettings['url'];
-                } else {
-                    $page = $client->get('spaces/' . $spaceId . '/stories/', [
-                        'contain_component' => $componentName,
-                        'is_published' => true,
-                        'per_page' => 1,
-                    ])->getBody();
-
-                    $componentPageUrl = $baseUrl . $page['stories'][0]['full_slug'];
-                }
-
-                if (array_key_exists('filename', $captureSettings)) {
-                    $filename = $screenshotPrefix . $captureSettings['filename'];
-                } else {
-                    $filename = $screenshotPrefix . Str::slug($componentName) . '.jpg';
-                }
-
-                try {
-                    BrowsershotLambda::url($componentPageUrl)
-                        ->setDelay($captureSettings['delay'] ?? 0)
-                        ->select($captureSettings['selector'])
-                        ->setScreenshotType('jpeg', 80)
-                        ->save(Storage::path($screenshotPath) . $filename);
-
-                    $imageDimensions = getimagesize(Storage::path($screenshotPath) . $filename);
-
-                    $fields = [
-                        'filename' => $filename,
-                        'asset_folder_id' => $assetFolder['id'],
-                        'size' => $imageDimensions[0] . 'x' . $imageDimensions[1],
-                    ];
-
-                    if ($component['image']) {
-                        $oldPreviewIdentifier = collect(explode('/', $component['image']))->slice(-2)->implode('/');
-                    } else {
-                        $oldPreviewIdentifier = null;
-                    }
-
-                    $signature = $client->post('spaces/' . $spaceId . '/assets', $fields)->getBody();
-
-                    $ch = curl_init($signature['post_url']);
-                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                    curl_setopt($ch, CURLOPT_HEADER, true);
-                    curl_setopt($ch, CURLOPT_POST, true);
-                    curl_setopt($ch, CURLOPT_POSTFIELDS, array_merge($signature['fields'], [
-                        'file' => new CurlFile(Storage::path($screenshotPath) . $filename, $signature['fields']['Content-Type'], $filename),
-                    ]));
-                    $response = curl_exec($ch);
-
-                    $lines = collect(explode("\r\n", $response))->filter(fn($line) => str_starts_with($line, 'Location: '))->implode('');
-
-                    $assetPreviewUrl =  urldecode(str_replace('https://s3.amazonaws.com/', '//', substr($lines, 10)));
-
-                    $component['image'] = $assetPreviewUrl;
-
-                    Components::make()->update($component['id'], $component);
-
-                    Storage::delete($screenshotPath . $filename);
-
-                    if ($oldPreviewIdentifier) {
-                        $oldPreviewAsset = $client->get('spaces/' . $spaceId . '/assets/', [
-                            'search' => $oldPreviewIdentifier,
-                        ])->getBody();
-
-                        if (count($oldPreviewAsset['assets'])) {
-                          //  $client->delete('/spaces/' . $spaceId . '/assets/' . $oldPreviewAsset['assets'][0]['id'])->getBody(); // TODO not working - 404 error
-                        }
-                    }
-
-                    $capturedComponents[] = [
-                        'component' => $componentName,
-                        'page' => $componentPageUrl,
-                        'selector' => $captureSettings['selector'],
-                        'image' => $assetPreviewUrl,
-                    ];
-
-                } catch (\Exception $e) {
-                   // $this->error('Error capturing: ' . $componentName . ' - selector: ' . $captureSettings['selector']);
-                   // $this->error($e->getMessage());
-
-                    $uncapturedComponents[] = [
-                        'component' => $componentName,
-                        'page' => $componentPageUrl,
-                        'selector' => $captureSettings['selector'],
-                        'error' => $e->getMessage(),
-                    ];
-                }
-            } else {
-                $uncapturedComponents[] = [
-                    'component' => $componentName,
-                    'selector' => $captureSettings['selector'],
-                    'page' => 'N/A',
-                    'error' => 'Component not found.',
-                ];
-            }
+    protected function processSingleComponent($componentName, $assetFolder, &$capturedComponents, &$uncapturedComponents)
+    {
+        if (array_key_exists($componentName, $this->toCapture) || in_array($componentName, $this->toCapture, true)) {
+            $result = $this->captureComponent($componentName, $assetFolder);
+            $capturedComponents = array_merge($capturedComponents, $result['captured']);
+            $uncapturedComponents = array_merge($uncapturedComponents, $result['uncaptured']);
+        } else {
+            $this->error('Component not found in configuration: ' . $componentName);
         }
+    }
 
+    protected function processAllComponents($assetFolder, &$capturedComponents, &$uncapturedComponents)
+    {
+        foreach ($this->toCapture as $componentName => $captureSettings) {
+            $result = $this->captureComponent($componentName, $assetFolder);
+            $capturedComponents = array_merge($capturedComponents, $result['captured']);
+            $uncapturedComponents = array_merge($uncapturedComponents, $result['uncaptured']);
+        }
+    }
+
+    protected function displayResults($capturedComponents, $uncapturedComponents)
+    {
         if (count($capturedComponents)) {
             $this->info('Captured components:');
             $this->table(['Component', 'Page', 'Selector', 'Image'], $capturedComponents, 'compact');
@@ -176,6 +106,150 @@ class CreateBlockPreviewsCommand extends Command
         if (count($uncapturedComponents)) {
             $this->info('Uncaptured components:');
             $this->table(['Component', 'Page', 'Selector', 'Error'], $uncapturedComponents, 'compact');
+        }
+    }
+
+    protected function captureComponent($componentName, $assetFolder)
+    {
+        $spaceId = config('storyblok-cli.space_id');
+        $baseUrl = config('storyblok-previews.base_url');
+
+        $captureSettings = $this->toCapture[$componentName];
+
+        $capturedComponents = [];
+        $uncapturedComponents = [];
+
+        $component = $this->getComponentData($componentName);
+
+        if ($component) {
+            $componentPageUrl = $this->getComponentPageUrl($componentName, $captureSettings, $baseUrl, $spaceId);
+
+            if (!$componentPageUrl) {
+                $this->warn('No page found for component: ' . $componentName);
+                return ['captured' => $capturedComponents, 'uncaptured' => $uncapturedComponents];
+            }
+
+            $filename = $this->getFilename($componentName, $captureSettings);
+
+            try {
+                $this->takeScreenshot($componentPageUrl, $captureSettings, $filename);
+                $assetPreviewUrl = $this->uploadScreenshot($filename, $assetFolder, $spaceId);
+                $this->updateComponentImage($component, $assetPreviewUrl, $spaceId);
+
+                $capturedComponents[] = [
+                    'component' => $componentName,
+                    'page' => $componentPageUrl,
+                    'selector' => $captureSettings['selector'],
+                    'image' => 'https:' . $assetPreviewUrl,
+                ];
+            } catch (\Exception $e) {
+                $uncapturedComponents[] = [
+                    'component' => $componentName,
+                    'page' => $componentPageUrl,
+                    'selector' => $captureSettings['selector'],
+                    'error' => $e->getMessage(),
+                ];
+            }
+        } else {
+            $uncapturedComponents[] = [
+                'component' => $componentName,
+                'selector' => $captureSettings['selector'],
+                'page' => 'N/A',
+                'error' => 'Component not found.',
+            ];
+        }
+
+        return ['captured' => $capturedComponents, 'uncaptured' => $uncapturedComponents];
+    }
+
+    protected function getComponentData($componentName)
+    {
+        $componentsData = Components::make()->all()->getComponents();
+        return $componentsData->where('name', $componentName)->first();
+    }
+
+    protected function getComponentPageUrl($componentName, $captureSettings, $baseUrl, $spaceId)
+    {
+        if (array_key_exists('url', $captureSettings)) {
+            return $baseUrl . $captureSettings['url'];
+        } else {
+            $page = $this->client->get('spaces/' . $spaceId . '/stories/', [
+                'contain_component' => $componentName,
+                'is_published' => config('storyblok-previews.only_published'),
+                'per_page' => 1,
+            ])->getBody();
+
+            if (!count($page['stories'])) {
+                return null;
+            } else {
+                return $baseUrl . $page['stories'][0]['full_slug'];
+            }
+        }
+    }
+
+    protected function getFilename($componentName, $captureSettings)
+    {
+        if (array_key_exists('filename', $captureSettings)) {
+            return config('storyblok-previews.prefix') . $captureSettings['filename'];
+        } else {
+            return config('storyblok-previews.prefix') . Str::slug($componentName) . '.jpg';
+        }
+    }
+
+    protected function takeScreenshot($componentPageUrl, $captureSettings, $filename)
+    {
+        $windowSize = $captureSettings['window_size'] ?? config('storyblok-previews.default_window_size');
+
+        BrowsershotLambda::url($componentPageUrl)
+            ->setDelay($captureSettings['delay'] ?? 0)
+            ->windowSize($windowSize[0], $windowSize[1])
+            ->select($captureSettings['selector'])
+            ->setScreenshotType('jpeg', 80)
+            ->save(Storage::path(config('storyblok-previews.storage_path')) . $filename);
+    }
+
+    protected function uploadScreenshot($filename, $assetFolder, $spaceId)
+    {
+        $imageDimensions = getimagesize(Storage::path(config('storyblok-previews.storage_path')) . $filename);
+
+        $fields = [
+            'filename' => $filename,
+            'asset_folder_id' => $assetFolder['id'],
+            'size' => $imageDimensions[0] . 'x' . $imageDimensions[1],
+        ];
+
+        $signature = $this->client->post('spaces/' . $spaceId . '/assets', $fields)->getBody();
+
+        $ch = curl_init($signature['post_url']);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, array_merge($signature['fields'], [
+            'file' => new CurlFile(Storage::path(config('storyblok-previews.storage_path')) . $filename, $signature['fields']['Content-Type'], $filename),
+        ]));
+        $response = curl_exec($ch);
+
+        $lines = collect(explode("\r\n", $response))->filter(fn($line) => str_starts_with($line, 'Location: '))->implode('');
+
+        Storage::delete(config('storyblok-previews.storage_path') . $filename);
+
+        return urldecode(str_replace('https://s3.amazonaws.com/', '//', substr($lines, 10)));
+    }
+
+    protected function updateComponentImage($component, $assetPreviewUrl, $spaceId)
+    {
+        $component['image'] = $assetPreviewUrl;
+        Components::make()->update($component['id'], $component);
+
+        if ($component['image']) {
+            $oldPreviewIdentifier = collect(explode('/', $component['image']))->slice(-2)->implode('/');
+            $oldPreviewAsset = $this->client->get('spaces/' . $spaceId . '/assets/', [
+                'search' => $oldPreviewIdentifier,
+            ])->getBody();
+
+            if (count($oldPreviewAsset['assets'])) {
+                // $this->client->delete('/spaces/' . $spaceId . '/assets/' . $oldPreviewAsset['assets'][0]['id'])->getBody(); // TODO not working - 404 error
+            }
         }
     }
 }
